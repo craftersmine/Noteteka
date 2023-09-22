c70f1cfeb887e360b0f245a150af387c5121d727
craftersmine <rodion.morozov.01@gmail.com>
2023-09-22 11:30:52 +0700

    Added Utilities class

---
+++ App/App/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+
+namespace App
+{
+    public class Utilities
+    {
+        public static T GetParent<T>(DependencyObject obj) => (T)GetParent(obj, typeof(T));
+        public static object GetParent(DependencyObject obj, Type type)
+        {
+            if (obj == null)
+                return null;
+
+            var parent = VisualTreeHelper.GetParent(obj);
+            if (parent == null)
+                return null;
+
+            if (type.IsAssignableFrom(parent.GetType()))
+                return parent;
+
+            return GetParent(parent, type);
+        }
+    }
+}
