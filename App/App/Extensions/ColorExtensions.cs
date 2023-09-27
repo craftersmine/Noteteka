332ae58a0329edc9ea5924e33377b828937dd739
craftersmine <rodion.morozov.01@gmail.com>
2023-09-27 22:31:18 +0700

    Added color extensions

---
+++ App/App/Extensions/ColorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace App.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color ToColor(this int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            return Color.FromArgb(bytes[3], bytes[2], bytes[1], bytes[0]);
+        }
+
+        public static int ToInt32(this Color value)
+        {
+            byte[] bytes = new byte[4];
+            bytes[2] = value.R;
+            bytes[1] = value.G;
+            bytes[0] = value.B;
+            bytes[3] = value.A;
+            return BitConverter.ToInt32(bytes);
+        }
+    }
+}
