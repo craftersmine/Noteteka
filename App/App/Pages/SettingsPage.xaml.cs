ad4e09cc606a880933ac8f70de57b3d52d4f3854
craftersmine <rodion.morozov.01@gmail.com>
2023-09-27 22:31:46 +0700

    Added settings page

---
+++ App/App/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,33 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnDatabasePurgeConfirmClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Windows.AppLifecycle.AppInstance.Restart("--purge-db");
+        }
+    }
+}
