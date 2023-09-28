1277fa0230807dee702d246611bd248019803996
craftersmine <rodion.morozov.01@gmail.com>
2023-09-29 00:20:57 +0700

    Added calendar related files

---
+++ App/App/Core/CalendarEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core
+{
+    public class CalendarEvent
+    {
+        public int Id { get; set; }
+        public DateTime EventDateTime { get; set; }
+        // TODO (craftersmine): fix db integration. implement JSON serialization?
+        public IEventRepeat RepeatAction { get; set; }
+        public string Description { get; set; }
+        public bool IsRepeating { get; set; }
+        public bool IsDone { get; set; }
+
+        public CalendarEvent()
+        {
+            EventDateTime = DateTime.Today;
+            Description = "New event";
+        }
+
+        public async void OnEventOccurred()
+        {
+            //CalendarEvent thisEvent = App.DatabaseContext.CalendarEvents.First(evt => evt.Id == this.Id);
+            if (IsRepeating)
+            {
+                EventDateTime = RepeatAction.NextEventOccurrence(this);
+
+                //thisEvent.EventDateTime = EventDateTime;
+            }
+            else
+            {
+                IsDone = true;
+                //thisEvent.IsDone = true;
+            }
+
+            //await App.DatabaseContext.SaveChangesAsync();
+        }
+    }
+}
