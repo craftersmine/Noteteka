f1cd415e5bc4a1cdd80ef34fa822860a72aba201
craftersmine <rodion.morozov.01@gmail.com>
2023-10-06 11:04:20 +0700

    Added Enum Extensions

---
+++ App/App/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T[] FromCommaSeparatedString<T>(this string value)
+        {
+            return value.Split(new[] { ',' })
+                .Select(e => Enum.Parse(typeof(T), e))
+                .Cast<T>()
+                .ToArray();
+        }
+
+        public static string ToCommaSeparatedString<T>(this T[] value) where T : Enum
+        {
+            return string.Join(",", value.Select(e => e.ToString("D")).ToArray());
+        }
+    }
+}
