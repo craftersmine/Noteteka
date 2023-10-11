fc1bf3158616ba0a3a1cd13646e00d496f770079
craftersmine <rodion.morozov.01@gmail.com>
2023-10-11 15:24:06 +0700

    Added ToDoTask class

---
+++ App/App/Core/ToDoTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core
+{
+    public class ToDoTask
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
