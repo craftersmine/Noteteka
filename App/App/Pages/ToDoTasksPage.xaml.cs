cbdcdba59e1d162d88c229fbfdb106004d759828
craftersmine <rodion.morozov.01@gmail.com>
2023-10-13 17:00:42 +0700

    Added task editor

--- App/App/Pages/ToDoTasksPage.xaml.cs
+++ App/App/Pages/ToDoTasksPage.xaml.cs
@@ -1,4 +1,5 @@
 using App.Core;
+using App.Dialogs;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml;
@@ -78,7 +79,7 @@ namespace App.Pages
                 App.DatabaseContext.SaveChanges();
                 ToDoEventsGridView.Visibility = Visibility.Visible;
                 //List<ToDoTask> tasks = App.DatabaseContext.ToDoTasks.ToList();
-                ToDoEventsGridView.ItemsSource = App.DatabaseContext.ToDoTasks.OrderBy(t => t.Priority).ToList();
+                ToDoEventsGridView.ItemsSource = App.DatabaseContext.ToDoTasks.OrderByDescending(t => t.Priority).ToList();
                 NoToDoEventsLabel.Visibility = Visibility.Collapsed;
             }
             else
@@ -88,17 +89,87 @@ namespace App.Pages
             }
         }
 
-        private void OnTaskPriorityChanged(object sender, SelectionChangedEventArgs e)
+        private void OnPriorityComboboxClosed(object sender, object e)
         {
             ComboBox comboBox = (ComboBox)sender;
             int taskId = (int)comboBox.Tag;
 
             ToDoTask task = App.DatabaseContext.ToDoTasks.FirstOrDefault(n => n.Id == taskId);
-            //task.Priority = (ToDoTaskPriority)comboBox.SelectedItem;
-            App.DatabaseContext.ToDoTasks.Entry(task).State = EntityState.Modified;
-            App.DatabaseContext.SaveChanges();
+
+            if (task.Priority != (ToDoTaskPriority)comboBox.SelectedItem)
+            {
+                //task.Priority = (ToDoTaskPriority)comboBox.SelectedItem;
+                App.DatabaseContext.ToDoTasks.Entry(task).State = EntityState.Modified;
+                App.DatabaseContext.SaveChanges();
+
+                UpdateToDoTasks();
+            }
+        }
+
+        private void AddTaskClick(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dlg = new ContentDialog();
+            AddEditTaskDialog dlgContent = new AddEditTaskDialog(null);
+            dlg.XamlRoot = this.XamlRoot;
+            dlg.Title = "Add new task";
+            dlg.Content = dlgContent;
+            dlg.CloseButtonText = "Cancel";
+            dlg.PrimaryButtonText = "Add";
+            dlg.CloseButtonClick += (dialog, args) => dialog.Hide();
+            dlg.PrimaryButtonClick += Dlg_PrimaryButtonClick;
+            dlg.ShowAsync();
+        }
+
+        private async void Dlg_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            AddEditTaskDialog dlgContent = (AddEditTaskDialog)sender.Content;
+
+            if (!dlgContent.IsEditing)
+            {
+                App.DatabaseContext.ToDoTasks.Add(new ToDoTask()
+                {
+                    Description = dlgContent.TaskDescription,
+                    Title = dlgContent.TaskTitle,
+                    Priority = dlgContent.TaskPriority
+                });
+            }
+            else
+            {
+                // TODO: fix editing of task
+                ToDoTask task = App.DatabaseContext.ToDoTasks.First(n => n.Id == dlgContent.ToDoTask.Id);
+                task.Title = dlgContent.TaskTitle;
+                task.Description = dlgContent.TaskDescription;
+                task.Priority = dlgContent.TaskPriority;
+                App.DatabaseContext.ToDoTasks.Entry(task).State = EntityState.Modified;
+
+            }
+            await App.DatabaseContext.SaveChangesAsync();
+            sender.PrimaryButtonClick -= Dlg_PrimaryButtonClick;
 
             UpdateToDoTasks();
+        }
+
+        private void ContextDeleteClick(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem item = sender as MenuFlyoutItem;
+        }
+
+        private void ContextEditClick(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem item = sender as MenuFlyoutItem;
+
+            ToDoTask task = App.DatabaseContext.ToDoTasks.FirstOrDefault(t => t.Id == (int)item.Tag);
+
+            ContentDialog dlg = new ContentDialog();
+            AddEditTaskDialog dlgContent = new AddEditTaskDialog(task);
+            dlg.XamlRoot = this.XamlRoot;
+            dlg.Title = "Edit task";
+            dlg.Content = dlgContent;
+            dlg.CloseButtonText = "Cancel";
+            dlg.PrimaryButtonText = "Edit";
+            dlg.CloseButtonClick += (dialog, args) => dialog.Hide();
+            dlg.PrimaryButtonClick += Dlg_PrimaryButtonClick;
+            dlg.ShowAsync();
         }
     }
 }
