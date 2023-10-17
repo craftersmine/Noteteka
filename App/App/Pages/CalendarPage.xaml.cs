719b060add11fe45e000b5e64c3b75ee45d07615
craftersmine <rodion.morozov.01@gmail.com>
2023-10-17 20:11:18 +0700

    Updates to calendar events

--- App/App/Pages/CalendarPage.xaml.cs
+++ App/App/Pages/CalendarPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using App.Core;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -26,6 +27,48 @@ namespace App.Pages
         public CalendarPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Sync()
+        {
+            App.DatabaseContext.CalendarEvents.Add(new CalendarEvent()
+            {
+                Title = "Electrotechnics",
+                Description = "307 - practice",
+                EventDateTime = new DateTime(2023, 10, 16, 20, 00, 00),
+                IsRepeating = true,
+                RepeatEvery = TimeSpan.FromDays(14),
+                RepeatOnDays = new DayOfWeek[] { DayOfWeek.Monday },
+                RemindBefore = TimeSpan.FromMinutes(30)
+            });
+            App.DatabaseContext.SaveChanges();
+        }
+
+        private void UpdateCalendarEvents()
+        {
+            CalendarEventsListBox.ItemsSource = null;
+            CalendarEventsListBox.Items.Clear();
+
+            if (App.DatabaseContext.CalendarEvents.Any())
+            {
+                DateTime nextWeek = DateTime.Now + TimeSpan.FromDays(7); 
+                App.DatabaseContext.SaveChanges();
+                CalendarEventsListBox.Visibility = Visibility.Visible;
+                CalendarEventsListBox.ItemsSource = App.DatabaseContext.CalendarEvents
+                    .Where(e => e.EventDateTime >= DateTime.Now).Where(e => e.EventDateTime <= nextWeek)
+                    .OrderByDescending(e => e.EventDateTime).ToList();
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            UpdateCalendarEvents();
+            base.OnNavigatedTo(e);
+        }
+
+        private void OnSyncClick(object sender, RoutedEventArgs e)
+        {
+            Sync();
         }
     }
 }
