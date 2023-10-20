0a3c81af360d57c9bda6249f2843e9822211e9a3
craftersmine <rodion.morozov.01@gmail.com>
2023-10-20 17:43:31 +0700

    Added DateTimeExtensions

---
+++ App/App/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime TrimSeconds(this DateTime d)
+        {
+            return new DateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, 0, 0);
+        }
+    }
+}
