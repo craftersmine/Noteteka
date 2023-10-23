c21bc3949e3b2b9b291e5b93ea3e34c16da23b8f
craftersmine <rodion.morozov.01@gmail.com>
2023-10-23 11:29:06 +0700

    Added notepad page db model

---
+++ App/App/Core/NotepadPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core
+{
+    public class NotepadPage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
