2b4b6106c385955f21cef508519365c12a68dc72
craftersmine <rodion.morozov.01@gmail.com>
2023-10-23 11:29:43 +0700

    Added notepad page editor

---
+++ App/App/Controls/NotepadPageEditor.xaml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using App.Core;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App.Controls
+{
+    public sealed partial class NotepadPageEditor : TabViewItem
+    {
+        public NotepadPage Page { get; private set; }
+
+        public NotepadPageEditor(NotepadPage page)
+        {
+            Page = page;
+
+            this.InitializeComponent();
+        }
+    }
+}
