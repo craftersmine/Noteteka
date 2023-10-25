e00336c8be08c7e25f5bb8c0b1d95c40491fe60d
craftersmine <rodion.morozov.01@gmail.com>
2023-10-25 16:53:02 +0700

    Added Db Metadata

---
+++ App/App/Core/DbMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Core
+{
+    public class DbMetadata
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid DbVersion { get; set; }
+    }
+}
--- App/App/Storage/ApplicationDatabaseContext.cs
+++ App/App/Storage/ApplicationDatabaseContext.cs
@@ -4,24 +4,40 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using App.Core;
 using App.Extensions;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace App.Storage
 {
     public class ApplicationDatabaseContext : DbContext
     {
+        private static readonly Guid DbVersion = new Guid("b8401604-a9f0-4c5c-98f3-e67d47c0e4dc");
+
         public string ApplicationDatabasePath { get; private set; }
 
         public DbSet<StickyNote> StickyNotes { get; set; }
         public DbSet<CalendarEvent> CalendarEvents { get; set; }
         public DbSet<ToDoTask> ToDoTasks { get; set; }
         public DbSet<NotepadPage> NotepadPages { get; set; }
+        public DbSet<DbMetadata> Metadata { get; set; }
 
         public ApplicationDatabaseContext()
         {
-            Database.EnsureCreated();
+            string filename = Path.Combine(App.ApplicationDataStoragePath, "DataStorage.db");
+            ApplicationDatabasePath = filename;
+
+            if (!File.Exists(filename))
+            {
+                Database.EnsureCreated();
+                Metadata.Add(new DbMetadata() { DbVersion = DbVersion });
+                SaveChanges();
+            }
+
+            if (Metadata.ToArray()[0].DbVersion != DbVersion)
+                Database.Migrate();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -34,9 +50,7 @@ namespace App.Storage
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string filename = Path.Combine(App.ApplicationDataStoragePath, "DataStorage.db");
-            ApplicationDatabasePath = filename;
-            optionsBuilder.UseSqlite("Filename=" + filename);
+            optionsBuilder.UseSqlite("Filename=" + ApplicationDatabasePath);
         }
     }
 }
